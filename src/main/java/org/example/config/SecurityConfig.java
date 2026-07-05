8b05b427c80998e01d62fa94bf6edf2c1ae7277b
ashish <official73ashish@gmail.com>
2026-07-05 17:05:09 +0530

    Permit home endpoint

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
 
                         .requestMatchers(
+                                "/",
                                 "/api/auth/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
